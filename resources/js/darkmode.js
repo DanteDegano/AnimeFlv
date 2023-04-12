66f95cf43f2d0461046d032476a7f5e45de951c8
DanteDegano <dantedegano@gmail.com>
2023-04-12 16:48:36 -0300

    Terminado el DarkMode

--- resources/js/darkmode.js
+++ resources/js/darkmode.js
@@ -1,5 +1,6 @@
 let myButton = document.getElementById("dm_btn");
 let container = document.getElementById("container");
+let login = document.getElementById("header_login");
 let container_wrp_left = document.getElementById("container_wrp_left");
 let container_wrp_right_title = document.querySelectorAll('.container_wrp_right_title');
 let container_wrp_left_title = document.getElementById("container_wrp_left_title");
@@ -12,6 +13,7 @@ myButton.addEventListener("click", function() {
     myButton.style.left = "35px";
     myButton.classList.add("darkmode");
     container.classList.add("darkmode");
+    login.classList.add("darkmode");
     container_wrp_left.classList.add("darkmode");
     container_wrp_left_title.classList.add("darkmode");
     list.classList.add("darkmode");
@@ -30,10 +32,12 @@ myButton.addEventListener("click", function() {
     myButton.style.left = "0px";
     myButton.classList.remove("darkmode");
     container.classList.remove("darkmode");
+    login.classList.remove("darkmode");
     container_wrp_left.classList.remove("darkmode");
     container_wrp_left_title.classList.remove("darkmode");
     list.classList.remove("darkmode");
 
+    // Recorre la lista de elementos que tienen la clase .section_wrap_2 y les va agregando/quitando uno por uno la clase 'darkmode'
     card.forEach((e)=>{
         e.classList.remove('darkmode')
     })
